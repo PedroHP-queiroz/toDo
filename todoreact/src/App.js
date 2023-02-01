1b91d39943f9b6ef21f5165d48ad5e662701c365
PEDRO HENRIQUE PEREIRA DE QUEIROZ <101682986+PedroHP-queiroz@users.noreply.github.com>
2023-02-01 15:25:39 -0300

    create

---
+++ todoreact/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FirstComponent from './components/FisrtComponent';
+import AnotherComponent from './components/AnotherComponent';
+import Hooks from './components/Hooks';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <FirstComponent />
+      <AnotherComponent />
+      <Hooks />
+    </div>
+  );
+}
+
+export default App;
